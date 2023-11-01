7d589111553c1089f5a50932cdbd34ab0c20298f
Bernardo Almeida <bernardoalmeida2004@gmail.com>
2023-11-01 13:02:36 +0000

    Finalização exercicio 5.

--- Caderno_Exericios/src/funcionario.cpp
+++ Caderno_Exericios/src/funcionario.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 Funcionario::Funcionario() {};
 
@@ -12,4 +13,57 @@ void Funcionario::ShowFuncionario() {
   Pessoa::Show();
   cout << "Setor: " << setor << endl;
   cout << "Num: " << num << endl;
+}
+
+istream& operator>>(istream& in, Funcionario& funcionario_temp) {
+  // Chama o >> sobrecarregado de Pessoa, static_cast converte de forma segura a pessoa para funcioario
+  in >> static_cast<Pessoa&>(funcionario_temp);
+
+  // Read data specific to Funcionario
+  cout << "Setor: ";
+  in >> funcionario_temp.setor;
+
+  cout << "Numero: ";
+  in >> funcionario_temp.num;
+  return in; 
+}
+
+void Funcionario::SaveFileFuncionario() {
+  Pessoa::SaveFilePessoa();
+  
+  string ficheiro;
+  cout << "Introduza o nome do ficheiro: " << endl;
+  cin >> ficheiro;
+
+  string nome_ficheiro = ficheiro + ".txt";
+
+  ofstream ficheiroFuncionario(nome_ficheiro, std::ios::app);
+  if (!ficheiroFuncionario.is_open()) {
+    cerr << "Erro ao abrir ficheiro" << endl;  
+    exit(1);
+  }
+  ficheiroFuncionario << setor << ";" << num << endl; 
+
+  ficheiroFuncionario.close();
+}
+
+void Funcionario::ReadFileFuncionario() {
+  string ficheiro;
+  cout << "Introduza o nome do ficheiro: " << endl;
+  cin >> ficheiro;
+
+  string nome_ficheiro = ficheiro + ".txt";
+
+  ifstream ficheiroFuncionario(nome_ficheiro);
+  if (!ficheiroFuncionario.is_open()) {
+    cerr << "Erro ao abrir o ficheiro" << endl; //cerr usado para mostrar erros
+    exit(1);
+  }
+  string linha_ficheiro;
+
+  while (getline(ficheiroFuncionario, linha_ficheiro)) {
+    cout << linha_ficheiro << endl;
+  }
+  ficheiroFuncionario.close();
+
 }
--- Caderno_Exericios/src/main.cpp
+++ Caderno_Exericios/src/main.cpp
@@ -1,6 +1,64 @@
 #include "C:\Users\ASUS\Desktop\projetos\CPP\Exercicios\include\funcionario.h"
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+void LerParaVetor(vector<Funcionario>& vetor_funcionarios) {
+  string ficheiro;
+  cout << "Introduza o nome do ficheiro: ";
+  cin >> ficheiro; 
+  string nome_ficheiro = ficheiro + ".txt";
+
+  ifstream ficheiroFuncionario(nome_ficheiro);
+
+  if (!ficheiroFuncionario.is_open()) {
+    cerr << "Erro ao abrir o ficheiro" << endl; //cerr usado para mostrar erros
+    exit(1);
+  }
+  string linha_ficheiro;
+  vector<string> funcionario_completo;
+  while (getline(ficheiroFuncionario, linha_ficheiro)) {
+
+    istringstream iss(linha_ficheiro);
+        
+    string pessoa_string;
+    while (getline(iss, pessoa_string, ';')) {
+      //Cada parte da string irá ser inserida num vetor
+      funcionario_completo.push_back(pessoa_string); 
+    }
+        
+        //stoi -> string to integer conversor
+    int dia_ficheiro = stoi(funcionario_completo[1]);
+    int mes_ficheiro = stoi(funcionario_completo[2]);
+    int ano_ficheiro = stoi(funcionario_completo[3]);
+      //Escolhemos cada parte do vetor consoante o seu conteudo. [0] -> Nome, [4] -> Morada
+    Data DataParaVetor(dia_ficheiro, mes_ficheiro, ano_ficheiro);
+
+    int num = stoi(funcionario_completo[6]);
+    Funcionario FuncionarioParaVetor(funcionario_completo[0], DataParaVetor, funcionario_completo[4], funcionario_completo[5], num);
+
+    vetor_funcionarios.push_back(FuncionarioParaVetor);
+    funcionario_completo.clear();
+
+  }
+  ficheiroFuncionario.close(); 
+}
+
+void OrdenarDataNascimento(vector<Funcionario> vetor_funcionarios) {
+  int vetor_size = size(vetor_funcionarios);
+  for ( int i = 0; i < vetor_size ; i++) {
+    for (int j = i +1; j < vetor_size; j++) {
+      Data data_j = vetor_funcionarios[j].getDataN();
+      Data data_i = vetor_funcionarios[i].getDataN();
+
+      if (data_j < data_i) {
+        swap(data_j, data_i);
+      }
+      
+    }
+  }
+}
 
 int main() {
 
@@ -14,10 +72,23 @@ int main() {
   VascoMaisCedo.SetSetor("Ferragem");
 
   Data data_temp;
-  //Alinea 5.6
+  //Alinea 5.7
   data_temp = BernardoAlmeida.getDataN();
   data_temp.SetDia(23);
   BernardoAlmeida.SetDataN(data_temp);
+
+  //Alinea 5.8
+  Funcionario Inacio;
+  cin >> Inacio;
+  Inacio.ShowFuncionario();
+
+  BernardoAlmeida.SaveFileFuncionario();
+  VascoMaisCedo.SaveFileFuncionario();
+  Inacio.SaveFileFuncionario();
+
+  vector<Funcionario> vetor_funcionarios;
+
+
   system("pause");
   return 0;
 }
